b39de9e814391929f4734e935fc80588d27b2d5e
Teltenkoetter <52072451+Teltenkoetter@users.noreply.github.com>
2026-04-23 12:28:54 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lernkarten-v6';
+const CACHE = 'lernkarten-v8';
 const SHELL = ['./index.html', './style.css', './app.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
